e9334a55bad81e9aef23497075ff31151c8cc67d
lsarteaga <lsarteaga_dev@protonmail.ch>
2021-07-21 10:49:10 -0500

    first commit

---
+++ src/main/java/com/example/segsw/SegswApplication.java
@@ -0,0 +1,13 @@
+package com.example.segsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegswApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SegswApplication.class, args);
+    }
+
+}
